f84045491287b27d5647bad8cfe488416cd2764e
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2026-02-10 20:40:21 +0800

    setup application logic and data objects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import "./style.css";
 
-console.log("Template is working!");
+const projectCollection = {};
+
+const createTodo = ({title, description, dueDate, priority}) => {
+    return {
+        title,
+        description,
+        dueDate,
+        priority
+    }
+};
+
+const createProject = (name) => {
+    projectCollection[name] = [];
+};
+
+const addToProject = (name, data) => {
+    const todo = createTodo(data);
+    projectCollection[name].push(todo);
+};
+
+const testProject = createProject('test');
+const testProject2 = createProject('anotherTest');
+
+addToProject('test', {
+    title: "Buy Eggs",
+    description: "Must Chicken Eggs",
+    dueDate: "Today",
+    priority: "Normal"
+});
+
+addToProject('anotherTest', {
+    title: "Buy Milk",
+    description: "Must Cow Milk",
+    dueDate: "Tomorrow",
+    priority: "Not Urgent"
+});
+
+ console.log(projectCollection);
