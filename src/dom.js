2c26d549b1880a121a0a2ed5da48470522643348
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2026-03-01 13:55:30 +0800

    Refactor codes using Single Responsibility Principle

--- src/dom.js
+++ src/dom.js
@@ -11,33 +11,37 @@ const createP = (text) => {
     const p = document.createElement('p');
     p.textContent = text;
     return p;
-}
+};
 
 const displayProject = () => {
     projectNames.forEach(item => {
-        const div = document.createElement('div');
-        div.classList.add('project-button');
+        const div = createNode('div', 'project-button', '');
         sidebar.appendChild(div);
-        const spanButton = document.createElement('span');
-        spanButton.textContent = item;
-        spanButton.classList.add('button', 'sidebar');
+
+        // Project Button Logic
+        const spanButton = createNode('span', 'button sidebar', item);
         spanButton.dataset.projectId = item.id;
         spanButton.addEventListener('click', getCurrentProject);
-        const closeProjectButton = document.createElement('span');
-        closeProjectButton.classList.add('close-project');
-        closeProjectButton.textContent = 'x';
-        closeProjectButton.addEventListener('click', (e) => {
-            sidebar.innerHTML = '';
-            mainContent.innerHTML = '';
-            delete projectCollection[item];
-            projectNames = Object.keys(projectCollection || {}); 
-            displayProject();
-        })
+
+        // Close Project Button Logic
+        const closeProjectButton = createNode('span', 'close-project', 'x');
+        closeProjectButton.addEventListener('click', () => {
+            handleCloseProjectButton(item)
+        });
+
         div.appendChild(closeProjectButton);
         div.appendChild(spanButton);
-    });
+    })
     saveToLocal();
-}
+};
+
+const handleCloseProjectButton = (targetItem) => {
+    sidebar.innerHTML = '';
+    mainContent.innerHTML = '';
+    delete projectCollection[targetItem];
+    projectNames = Object.keys(projectCollection || {}); 
+    displayProject();
+};
 
 const getCurrentProject = (e) => {
     currentProject = e.target.textContent;
@@ -45,122 +49,98 @@ const getCurrentProject = (e) => {
     displayTodo();
 };
 
+const createNode = (type, className, text) => {
+    const element = document.createElement(type);
+    
+    if (className) {
+        const classArray = className.split(' ');
+        element.classList.add(...classArray);
+    }
+    if (text) {
+        element.textContent = text;
+    }
+    
+    return element;
+};
+
 const displayTodo = () => {
     mainContent.innerHTML = '';
-    projectCollection[currentProject].forEach(todo => { 
-        const titleLabel = document.createElement('div');
-        titleLabel.classList.add('title');
+    projectCollection[currentProject].forEach(todo => {
+        // Main Task Logic
+        const titleLabel = createNode('div', 'title', '');
         mainContent.appendChild(titleLabel);
+        const spanTitle = createNode('span', 'title-checkmark', todo.title);
+        titleLabel.appendChild(spanTitle);
+        titleLabel.appendChild(createP(todo.dueDate));
+        titleLabel.appendChild(createP(todo.priority));
 
+        const divPanel = createNode('div', 'panel active', '');
+        mainContent.appendChild(divPanel);
+        
+        // Checkbox Logic
         const inputCheckbox = document.createElement('input');
         inputCheckbox.type = 'checkbox';
         inputCheckbox.dataset.todoId = todo.id;
         inputCheckbox.addEventListener('click', () => {
-            if (inputCheckbox.checked) {
-                todo.isComplete = !todo.isComplete;
-                saveToLocal();
-            } else {
-                todo.isComplete = false;
-                saveToLocal();
-            }
-        })
+            handleCheckbox(inputCheckbox, todo)
+        });
         titleLabel.prepend(inputCheckbox);
-
         if (todo.isComplete) {
             inputCheckbox.checked = true;
         }
 
-        const spanTitle = document.createElement('span');
-        spanTitle.classList.add('title-checkmark');
-        spanTitle.textContent = todo.title;
-        titleLabel.appendChild(spanTitle);
-
-        const divPanel = document.createElement('div');
-        divPanel.classList.add('panel', 'active');
-        const titleBtn = document.createElement('button');
-
-        const expandIcon = document.createElement('span');
-        expandIcon.classList.add('mdi', 'mdi-chevron-right-circle');
+        // Accordion Logic
+        const titleBtn = createNode('button', 'title-button', '');
+        const expandIcon = createNode('span', 'mdi mdi-chevron-right-circle', '');
         expandIcon.style.cursor = "pointer";
         expandIcon.style.fontSize = "24px";
         titleBtn.appendChild(expandIcon);
-
-        const minimiseIcon = document.createElement('span');
-        minimiseIcon.classList.add('mdi', 'mdi-chevron-down-circle');
+        const minimiseIcon = createNode('span', 'mdi mdi-chevron-down-circle', '');
         minimiseIcon.style.cursor = "pointer";
         minimiseIcon.style.fontSize = "24px";
         titleBtn.appendChild(minimiseIcon);
-
-        titleBtn.classList.add('title-button');
+        titleBtn.addEventListener('click', () => {
+            handleAccordion(divPanel, expandIcon, minimiseIcon)
+        });
         if (divPanel.classList.contains('active')) {
             divPanel.style.maxHeight = divPanel.scrollHeight + "px";
             expandIcon.style.display = 'none';
             minimiseIcon.style.display = 'block';
         }
-        titleBtn.addEventListener('click', () => {
-            divPanel.classList.toggle('active');
-            if (divPanel.classList.contains('active')) {
-                divPanel.style.maxHeight = divPanel.scrollHeight + "px";
-                expandIcon.style.display = 'none';
-                minimiseIcon.style.display = 'block';
-            } else {
-                divPanel.style.maxHeight = "0px";
-                expandIcon.style.display = 'block';
-                minimiseIcon.style.display = 'none';
-            }
-        })
         titleLabel.appendChild(titleBtn);
-  
-        mainContent.appendChild(divPanel);
-        const divSubTask = document.createElement('div');
-        divSubTask.classList.add('divSubTask');
-        divPanel.appendChild(divSubTask);
+        
 
-        const addSubTask = document.createElement('button');
-        addSubTask.classList.add('add-subTask');
-        addSubTask.textContent = '+';
+        // Subtask Logic
+        const divSubTask = createNode('div', 'divSubTask', '');
+        divPanel.appendChild(divSubTask);
+        const addSubTask = createNode('button', 'add-subTask', '+');
         todo.subTask.forEach(item => renderSubTask(item, divSubTask, todo, divPanel));
-        addSubTask.addEventListener('click', (event) => {
-            event.stopPropagation();
-            renderSubTask('',divSubTask, todo, divPanel);
-            if (!divPanel.classList.contains('active')) {
-                divPanel.classList.toggle('active');
-                divPanel.style.maxHeight = divPanel.scrollHeight + "px";
-                expandIcon.style.display = 'none';
-                minimiseIcon.style.display = 'block';
-            }
+        addSubTask.addEventListener('click', (e) => {
+            handleSubTask(e, divSubTask, todo, divPanel, expandIcon, minimiseIcon)
         });
         titleBtn.appendChild(addSubTask);
 
-        const divDescription = document.createElement('div');
-        divDescription.classList.add('divDescription');
+        // Description Logic
+        const divDescription = createNode('div', 'divDescription', '');
         divPanel.appendChild(divDescription);
-        const formDescription = document.createElement('form');
+        const saveDescription = createNode('button', '', 'Save');
         const descriptionLabel = document.createElement('label');
-        descriptionLabel.for = 'description';
-        divDescription.appendChild(formDescription);
+        const formDescription = document.createElement('form');
         const descriptionInput = document.createElement('input');
         descriptionInput.type = 'text';
         descriptionInput.id = 'description';
         descriptionInput.name = 'description';
         descriptionInput.placeholder = 'Add description...';
         todo.description === undefined ? descriptionInput.value = '': descriptionInput.value = todo.description;
-        const saveDescription = document.createElement('button');
-        saveDescription.textContent = 'Save';
-        formDescription.addEventListener('submit', (event) => {
-            event.preventDefault();
-            todo.description = descriptionInput.value;
-            saveToLocal();
+        formDescription.addEventListener('submit', (e) => {
+            handleDescription(e, todo, descriptionInput)
         });
+        divDescription.appendChild(formDescription);
         formDescription.appendChild(descriptionLabel);
         descriptionLabel.appendChild(saveDescription);
         descriptionLabel.appendChild(descriptionInput);
-        titleLabel.appendChild(createP(todo.dueDate));
-        titleLabel.appendChild(createP(todo.priority));
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete Task';
-        deleteBtn.classList.add('button', 'delete');
+        const deleteBtn = createNode('button', 'button delete', 'Delete Task');
         deleteBtn.dataset.todoId = todo.id;
         deleteBtn.addEventListener('click', handleRemove);
         mainContent.appendChild(deleteBtn);
@@ -169,14 +149,56 @@ const displayTodo = () => {
         if (openPanel) {
             openPanel.forEach(ele => ele.style.maxHeight = ele.scrollHeight + "px");
         }
-    })            
-}
+    })
+};
+
 const handleRemove = (e) => {
     const idToDelete = e.target.dataset.todoId;
     projectCollection[currentProject] = projectCollection[currentProject].filter(item => item.id !== idToDelete);
     displayTodo();
     saveToLocal();
-}
+};
+
+const handleDescription = (e, targetTask, targetEle) => {
+    e.preventDefault();
+    targetTask.description = targetEle.value;
+    saveToLocal();
+};
+
+const handleSubTask = (e, targetDiv, targetTask, targetPanel, targetIcon1, targetIcon2) => {
+    e.stopPropagation();
+    renderSubTask('', targetDiv, targetTask, targetPanel);
+    if (!targetPanel.classList.contains('active')) {
+        targetPanel.classList.toggle('active');
+        targetPanel.style.maxHeight = targetPanel.scrollHeight + "px";
+        targetIcon1.style.display = 'none';
+        targetIcon2.style.display = 'block';
+    }
+};
+
+const handleCheckbox = (target, targetToDo) => {
+    if (target.checked) {
+        targetToDo.isComplete = !targetToDo.isComplete;
+        saveToLocal();
+    } else {
+        targetToDo.isComplete = false;
+        saveToLocal();
+    }
+};
+
+const handleAccordion = (targetPanel, targetIcon1, targetIcon2) => {
+    targetPanel.classList.toggle('active');
+    if (targetPanel.classList.contains('active')) {
+        targetPanel.style.maxHeight = targetPanel.scrollHeight + "px";
+        targetIcon1.style.display = 'none';
+        targetIcon2.style.display = 'block';
+    } else {
+        targetPanel.style.maxHeight = "0px";
+        targetIcon1.style.display = 'block';
+        targetIcon2.style.display = 'none';
+    }
+};
+
 const insertTodo = (event) => {
     event.preventDefault();
     if (!currentProject) {
@@ -197,10 +219,11 @@ const insertTodo = (event) => {
 const renderSubTask = (item, targetContainer, targetTask, targetContainer2) => {
     let newsubTask;
     const formSubTask = document.createElement('form');
-    const labelSubTask = document.createElement('label');
-    labelSubTask.for = 'subtask';
     targetContainer.appendChild(formSubTask);
+    const labelSubTask = document.createElement('label');
     formSubTask.appendChild(labelSubTask);
+
+    // InputSubTask Logic
     const inputSubTask = document.createElement('input');
     inputSubTask.type = 'text';
     inputSubTask.id = 'subtask';
@@ -210,94 +233,118 @@ const renderSubTask = (item, targetContainer, targetTask, targetContainer2) => {
         inputSubTask.value = ''  
     } else if (targetTask.subTask.length > 0) {
         inputSubTask.value = item.title || '';
-    }
+    };
     labelSubTask.appendChild(inputSubTask);
+
+    // SubTask Checkbox Logic
     const subTaskCheckbox = document.createElement('input');
     subTaskCheckbox.type = 'checkbox';
     subTaskCheckbox.addEventListener('click', () => {
-        if (newsubTask) {
-            if (newsubTask.id !== inputSubTask.dataset.subTaskid && subTaskCheckbox.checked === true) {
-                item.isComplete = !item.isComplete;
-                saveToLocal();
-            } else if (newsubTask.id === inputSubTask.dataset.subTaskid && subTaskCheckbox.checked === true ) {
-                newsubTask.isComplete = !newsubTask.isComplete;
-                saveToLocal();
-            } else {
-                newsubTask.isComplete = false;
-                saveToLocal();
-            }
-        } else {
-            if (subTaskCheckbox.checked === true) {
-                item.isComplete = !item.isComplete;
-                saveToLocal();
-            } else {
-                item.isComplete = false;
-                saveToLocal();
-            }  
-        }
-    })
+        handleSubTaskCheckBox(newsubTask, inputSubTask, subTaskCheckbox, item)
+    });
     if (/\d/.test(inputSubTask.dataset.subTaskid)) {
         labelSubTask.prepend(subTaskCheckbox);
-    }
+    };
     if (item.isComplete) {
         subTaskCheckbox.checked = true;
-    }
-    const saveSubTask = document.createElement('button');
-    saveSubTask.textContent = 'Save';
+    };
+
+    // Save SubTask Logic
+    const saveSubTask = createNode('button', '', 'Save');
     labelSubTask.appendChild(saveSubTask);
-    const editSubTask = document.createElement('button');
-    editSubTask.textContent = 'Edit';
+
+    // Edit SubTask Logic
+    const editSubTask = createNode('button', '', 'Edit');
     if (inputSubTask.value !== '') {
         editSubTask.disabled = false;
         saveSubTask.disabled = true;
     } else {
         editSubTask.disabled = true;
         saveSubTask.disabled = false;
-    }
-    const closeSubTaskButton = document.createElement('span');
-    closeSubTaskButton.classList.add('close-subtask');
-    closeSubTaskButton.textContent = 'x';
-    closeSubTaskButton.addEventListener('click', (e) => {
-        if (inputSubTask.dataset.subTaskid) {
-            const idToDelete = inputSubTask.dataset.subTaskid;
-            targetTask.subTask = targetTask.subTask.filter(item => item.id !== idToDelete);
-            e.target.parentElement.parentElement.remove();
-            saveToLocal();
-        } else {
-            e.target.parentElement.parentElement.remove();
-        }
-    })
-    editSubTask.addEventListener('click', (event) => {
-        event.preventDefault();
-        saveSubTask.disabled = false;
-    })
+    };
+    editSubTask.addEventListener('click', (e) => {
+        handleEditSubTask(e, saveSubTask)
+    });
     labelSubTask.appendChild(editSubTask);
-    formSubTask.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const foundSubTask = targetTask.subTask.find(obj => obj.id === inputSubTask.dataset.subTaskid || '');
-        if (foundSubTask) {
-            foundSubTask.title = inputSubTask.value;
-            saveSubTask.disabled = true;
+
+    // Close SubTaskButton Logic
+    const closeSubTaskButton = createNode('span', 'close-subtask', 'x');
+    closeSubTaskButton.addEventListener('click', (e) => {
+        handleCloseSubTaskButton(e, inputSubTask, targetTask)
+    });
+    labelSubTask.appendChild(closeSubTaskButton);
+
+    // Form Submit Logic
+    formSubTask.addEventListener('submit', (e) => {
+        newsubTask = handleFormSubmit(e, targetTask, inputSubTask, saveSubTask, newsubTask, closeSubTaskButton, labelSubTask, subTaskCheckbox, editSubTask)
+    });
+
+    targetContainer2.style.maxHeight = targetContainer2.scrollHeight + "px";
+}; 
+
+const handleFormSubmit = (e, targetTask2, targetInput, targetSubTaskButton, targetNewSubTask, targetSubTaskButton2, targetLabel, targetSTCheckbox, targetEditST) => {
+    e.preventDefault();
+    const foundSubTask = targetTask2.subTask.find(obj => obj.id === targetInput.dataset.subTaskid || '');
+    if (foundSubTask) {
+        foundSubTask.title = targetInput.value;
+        targetSubTaskButton.disabled = true;
+        saveToLocal();
+    } else {
+        const subTaskData = {
+                title: targetInput.value,
+            }
+        targetNewSubTask = createSubTask(subTaskData);
+        targetInput.dataset.subTaskid = targetNewSubTask.id;
+        addToToDo(targetTask2.subTask, targetNewSubTask);
+        targetSubTaskButton.disabled = true; 
+        targetSubTaskButton2.dataset.subTaskid = targetNewSubTask.id;
+        targetLabel.prepend(targetSTCheckbox);
+    }
+    if (targetSubTaskButton.disabled) {
+        targetEditST.disabled = false;
+    } 
+    saveToLocal(); 
+    return targetNewSubTask; 
+};
+
+const handleEditSubTask = (e, targetButton) => {
+    e.preventDefault();
+    targetButton.disabled = false;
+};
+
+const handleCloseSubTaskButton = (e, targetInput, targetTask2) => {
+    if (targetInput.dataset.subTaskid) {
+        const idToDelete = targetInput.dataset.subTaskid;
+        targetTask2.subTask = targetTask2.subTask.filter(item => item.id !== idToDelete);
+        e.target.parentElement.parentElement.remove();
+        saveToLocal();
+    } else {
+        e.target.parentElement.parentElement.remove();
+    }
+};
+
+const handleSubTaskCheckBox = (targetSubTask, targetInput, targetSubTaskCheckbox, targetItem ) => {
+    if (targetSubTask) {
+        if (targetSubTask.id !== targetInput.dataset.subTaskid && targetSubTaskCheckbox.checked === true) {
+            targetItem.isComplete = !targetItem.isComplete;
+            saveToLocal();
+        } else if (targetSubTask.id === targetInput.dataset.subTaskid && targetSubTaskCheckbox.checked === true ) {
+            targetSubTask.isComplete = !targetSubTask.isComplete;
             saveToLocal();
         } else {
-            const subTaskData = {
-                    title: inputSubTask.value,
-                }
-            newsubTask = createSubTask(subTaskData);
-            inputSubTask.dataset.subTaskid = newsubTask.id;
-            addToToDo(targetTask.subTask, newsubTask);
-            saveSubTask.disabled = true; 
-            closeSubTaskButton.dataset.subTaskid = newsubTask.id;
-            labelSubTask.prepend(subTaskCheckbox);
+            targetSubTask.isComplete = false;
+            saveToLocal();
         }
-        if (saveSubTask.disabled) {
-            editSubTask.disabled = false;
-        } 
-        saveToLocal();  
-    })
-    labelSubTask.appendChild(closeSubTaskButton);
-    targetContainer2.style.maxHeight = targetContainer2.scrollHeight + "px";
-}
+    } else {
+        if (targetSubTaskCheckbox.checked === true) {
+            targetItem.isComplete = !targetItem.isComplete;
+            saveToLocal();
+        } else {
+            targetItem.isComplete = false;
+            saveToLocal();
+        }  
+    }
+};
     
 form.addEventListener('submit', insertTodo);
 addProject.addEventListener('click', () => {
@@ -310,7 +357,7 @@ addProject.addEventListener('click', () => {
         projectNames = Object.keys(projectCollection || {});
         displayProject();
     }
-})
+});
 
 export { projectNames, displayProject, currentProject };
 
